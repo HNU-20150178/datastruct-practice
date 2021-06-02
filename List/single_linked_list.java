cf388179e22e9f53680b8613bedb643fdbc85eab
Yoon <organicferrum@gmail.com>
2021-06-02 17:43:17 +0900

    chain.Java add

---
+++ List/single_linked_list.java
@@ -0,0 +1,22 @@
+package List;
+
+import java.util.LinkedList;
+
+public class single_linked_list {
+	public static void main(String[] args) {
+		LinkedList<Integer> llist = new LinkedList<Integer>();	
+	    llist.addFirst(1);
+	    llist.addLast(2);
+	    llist.add(3);
+	    llist.add(1,10); //index 1 뒤에 10추가
+	    
+		
+	    for(Integer i : llist) {
+	    	System.out.println(i);
+	    }
+	    
+	    llist.clear();
+
+	}
+
+}
